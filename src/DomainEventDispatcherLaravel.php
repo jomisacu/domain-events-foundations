af9b5a58c51b7c55cf99eb28fbe2604128e243be
jomisacu <jomisacu.software@gmail.com>
2025-04-25 19:07:22 -0400

    Refactor event dispatching logic in DomainEventDispatcherLaravel
    Introduce delayed dispatching based on environment context. Events are now dispatched after commands finish or at the end of a request lifecycle, ensuring proper timing and reducing potential issues in execution. Added class-level docblock for clarity and documentation.

--- src/DomainEventDispatcherLaravel.php
+++ src/DomainEventDispatcherLaravel.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Jomisacu\DomainEventsFoundations;
 
+/**
+ * DomainEventDispatcherLaravel is a concrete implementation of the DomainEventDispatcherInterface
+ * that uses Laravel's event system to dispatch domain events.
+ *
+ * This class allows you to dispatch domain events using Laravel's event system
+ * and store them in a repository.
+ *
+ * If you are running in CLI context it will dispatch the event when the command is finished.
+ * If you are running in a web context it will dispatch the event when the request is finished.
+ *
+ * @package Jomisacu\DomainEventsFoundations
+ */
 final class DomainEventDispatcherLaravel implements DomainEventDispatcherInterface
 {
     public function __construct(private readonly DomainEventRepositoryInterface $domainEventRepository)
@@ -14,7 +26,31 @@ final class DomainEventDispatcherLaravel implements DomainEventDispatcherInterfa
     {
         $this->domainEventRepository->save($domainEvent);
 
-        // Assuming you are using Laravel's event system
-        \Event::dispatch($domainEvent);
+        $this->dispatchAfter($domainEvent);
+    }
+
+    private function dispatchAfter(DomainEventInterface $domainEvent): void
+    {
+        if (\App::runningInConsole()) {
+            $this->dispatchWhenCommandIsFinished($domainEvent);
+
+            return;
+        }
+
+        $this->dispatchWhenRequestIsFinished($domainEvent);
+    }
+
+    private function dispatchWhenCommandIsFinished(DomainEventInterface $domainEvent): void
+    {
+        \Event::listen(\Illuminate\Console\Events\CommandFinished::class, function () use ($domainEvent) {
+            \Event::dispatch($domainEvent);
+        });
+    }
+
+    private function dispatchWhenRequestIsFinished(DomainEventInterface $domainEvent): void
+    {
+        \App::terminating(function () use ($domainEvent) {
+            \Event::dispatch($domainEvent);
+        });
     }
 }
