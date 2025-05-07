e516158267462e1cbf76e90e9c858df85980fb18
jomisacu <jomisacu.software@gmail.com>
2025-05-07 12:40:50 -0400

    Add async domain event dispatcher using Laravel lifecycle
    Introduce `DomainEventDispatcherLaravelAsync` to handle domain event dispatching asynchronously. It schedules events for dispatching either at the end of a web request or after CLI command execution, ensuring a non-blocking event-driven workflow. Events are stored in a repository before being dispatched.

---
+++ src/DomainEventDispatcherLaravelAsync.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jomisacu\DomainEventsFoundations;
+
+use Illuminate\Queue\Jobs\Job;
+
+/**
+ * This class allows you to dispatch domain events using Laravel's event system using a scheduled job
+ * and store them in a repository.
+ *
+ * If you are running in CLI context it will dispatch the event when the command is finished.
+ * If you are running in a web context it will dispatch the event when the request is finished.
+ *
+ * @package Jomisacu\DomainEventsFoundations
+ */
+final class DomainEventDispatcherLaravelAsync implements DomainEventDispatcherInterface
+{
+    public function __construct(private readonly DomainEventRepositoryInterface $domainEventRepository)
+    {
+    }
+
+    public function dispatch(DomainEventInterface $domainEvent): void
+    {
+        $this->domainEventRepository->save($domainEvent);
+
+        $this->dispatchAfter($domainEvent);
+    }
+
+    private function dispatchAfter(DomainEventInterface $domainEvent): void
+    {
+        if (\App::runningInConsole()) {
+            $this->dispatchWhenCommandIsFinished($domainEvent);
+
+            return;
+        }
+
+        $this->dispatchWhenRequestIsFinished($domainEvent);
+    }
+
+    private function dispatchWhenCommandIsFinished(DomainEventInterface $domainEvent): void
+    {
+        \Event::listen(\Illuminate\Console\Events\CommandFinished::class, function () use ($domainEvent) {
+            \Schedule::job(function (Job $job) use ($domainEvent) {
+                \Event::dispatch($domainEvent);
+                $job->delete();
+            });
+        });
+    }
+
+    private function dispatchWhenRequestIsFinished(DomainEventInterface $domainEvent): void
+    {
+        \App::terminating(function () use ($domainEvent) {
+            \Schedule::job(function (Job $job) use ($domainEvent) {
+                \Event::dispatch($domainEvent);
+                $job->delete();
+            });
+        });
+    }
+}
